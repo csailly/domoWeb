eeea88caaee4d63c5d517e31a5c34510ab931753
nestof <nestof@gmail.com>
2014-09-17 16:55:48 +0200

    Nettoyage de la barre de navigation

--- WebContent/include/navbar.php
+++ WebContent/include/navbar.php
@@ -13,8 +13,8 @@
 		<div class="navbar-collapse collapse">
 			<ul class="nav navbar-nav">
 				<li class="active"><a href="/index.php">Home</a></li>
-				<li><a href="#about">About</a></li>
-				<li><a href="#contact">Contact</a></li>
+<!-- 				<li><a href="#about">About</a></li> -->
+<!-- 				<li><a href="#contact">Contact</a></li> -->
 				<li class="dropdown"><a href="#" class="dropdown-toggle"
 					data-toggle="dropdown">Menu <b class="caret"></b></a>
 					<ul class="dropdown-menu">
