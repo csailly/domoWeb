1e7b88e809103a54b77aa0fd1250ed9a5a4ce225
nestof <nestof@gmail.com>
2015-05-31 20:14:35 +0200

    Changes from pi

--- conf/DomoWebConfig.php
+++ conf/DomoWebConfig.php
@@ -2,14 +2,14 @@
 class DomoWebConfig {
 	private $databaseDirectory;
 	private $databaseFilename;
-	
-	public $databaseConnexion;	
+
+	public $databaseConnexion;
 	public $externalCommandTemp;
 	public $externalCommandMcz;
 	public $externalCommandUpdateDomoWeb;
 	public $externalCommandUpdateDomoCore;
-	
-	
+
+
 	function __construct(){
 		/* Configuration de la base de données */
 		if (PHP_OS == "Linux"){
@@ -19,31 +19,32 @@ class DomoWebConfig {
 			$this->databaseDirectory = dirname(__FILE__).'/../';
 			$this->databaseFilename = 'domotique.sqlite';
 		}
-		
+
 		try	{
 			$this->databaseConnexion = new PDO('sqlite:'.$this->databaseDirectory.$this->databaseFilename);
 			$this->databaseConnexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}catch(Exception $e){
+			echo 'Configuration de la base de donnée <br/>';
 			echo 'Erreur : '.$e->getMessage().'<br />';
 			echo 'N° : '.$e->getCode();
 			die();
 		}
-		
+
 		/* Les commandes externes */
 		if (PHP_OS == "Linux"){
-			$this->externalCommandTemp = 'python /home/pi/scripts/domoCore/com/nestof/domocore/service/TempService.py';
-			$this->externalCommandMcz = 'sudo python /home/pi/scripts/domoCore/run.py  >> /home/pi/log/domocore.log 2>&1';
+			$this->externalCommandTemp = 'python /home/pi/apps/domoCore/com/nestof/domocore/service/TempService.py';
+			$this->externalCommandMcz = 'sudo python /home/pi/apps/domoCore/run.py';
 			$this->externalCommandUpdateDomoWeb = 'sh '.__DIR__.'/update.sh';
-			$this->externalCommandUpdateDomoCore = 'sudo sh /home/pi/scripts/domoCore/conf/update.sh';
+			$this->externalCommandUpdateDomoCore = 'sudo sh /home/pi/apps/domoCore/conf/update.sh';
 		}else{
 			$this->externalCommandTemp = 'C:\Python34\python.exe D:\Documents\Work\domocore\com\nestof\domocore\service\TempServiceDev.py';
 			$this->externalCommandMcz = 'C:\Python34\python.exe D:\Documents\Work\domocore\run.py';
-		
+
 			//$this->externalCommandTemp = 'D:\+sandbox\python3.4.0\python.exe D:\+sandbox\work\domocore\com\nestof\domocore\service\TempServiceDev.py';
 			//$this->externalCommandMcz = 'D:\+sandbox\python3.4.0\python.exe D:\+sandbox\work\domocore\run.py';
 			$this->externalCommandUpdateDomoWeb = __DIR__.'\updateWebApp.sh';
 			$this->externalCommandUpdateDomoCore = 'sudo python /home/pi/scripts/domoCore/conf/update.sh';
 		}
 	}
-}		
+}
 ?>
--- dao/TemperatureDAO.php
+++ dao/TemperatureDAO.php
@@ -2,15 +2,15 @@
 include_once $_SERVER ['DOCUMENT_ROOT'] . '/dao/GenericDAO.php';
 class TemperatureDAO extends GenericDAO {
 	/**
-	 * 
+	 *
 	 * @param DateTime $startDate
 	 * @return multitype:number multitype:number
 	 */
 	function getAllTemperatures($startDate, $sonde) {
 		date_default_timezone_set ( 'Europe/Paris' );
-	
+
 		$queryString = "SELECT * FROM histo_temp h  where date >= :startDate and sonde = :sonde order by date, heure";
-		
+
 		$stmt = $this->connexion->prepare ( $queryString ); // on va chercher tous les membres de la table qu'on trie par ordre croissant
 		$stmt->execute ( array (
 				'startDate' => $startDate->format('Y-m-d'),
@@ -18,24 +18,24 @@ class TemperatureDAO extends GenericDAO {
 		) );
 		$stmt->setFetchMode ( PDO::FETCH_OBJ ); // on dit qu'on veut que le résultat soit récupérable sous forme d'objet
 		$ligne = $stmt->fetch ();
-		
-		
-		$startDateTime = $ligne->date.'T'.$ligne->heure.' UTC';
-		$startDateTime_ms = 1000 * strtotime($startDateTime);
-		
+
+		if($ligne){
+			$startDateTime = $ligne->date.'T'.$ligne->heure.' UTC';
+			$startDateTime_ms = 1000 * strtotime($startDateTime);
+		}
+
 		while ( $ligne ) 		// on récupère la liste des membres
 		{
 			$datas [] = array (
 					1000 * strtotime ( $ligne->date . 'T' . $ligne->heure . ' UTC' ),
-					1 * $ligne->temp 
+					1 * $ligne->temp
 			);
-			
 			$ligne = $stmt->fetch ();
 		}
 		$stmt->closeCursor (); // on ferme le curseur des résultats
-		
+
 		$result = array('startTime' => $startDateTime_ms, 'datas' => $datas);
-		
+
 		return $result;
 	}
 }
